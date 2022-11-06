e83a9c7c67d3e5384282e5006acaa754795fc18a
ko0hi <ko0hi.4731@gmail.com>
2022-11-07 00:18:15 +0900

    pluginsパッケージのベースモジュールをリファクタ

--- pybotters_wrapper/plugins/__init__.py
+++ pybotters_wrapper/plugins/__init__.py
@@ -1,3 +1,105 @@
-from ._base import WatchPlugin
-from .bar import TimeBarStreamDataFrame
-from .book import Book
+from typing import Callable
+
+import importlib
+
+import pandas as pd
+
+from ._base import DataStorePlugin, MultipleDataStoresPlugin
+from .bar import TimeBarStreamDataFrame, VolumeBarStreamDataFrame
+from .binning_book import BinningBook
+from .book_ticker import BookTicker
+
+from pybotters_wrapper.common import DataStoreManagerWrapper
+
+
+def _import_module(store):
+    try:
+        module_path = f"pybotters_wrapper.{store.exchange}.plugins"
+        return importlib.import_module(module_path)
+    except ModuleNotFoundError as e:
+        raise RuntimeError(f"no plugins module for {store.exchange}")
+
+
+def _build_plugin(store, module, name, **kwargs):
+
+    try:
+        return getattr(module, name)(store, **kwargs)
+    except AttributeError:
+        raise RuntimeError(
+            f"'{name}' plugin has not been supported yet for {store.exchange}"
+        )
+
+
+def _import_and_build_plugin(store, name, *, default_cls=None, **kwargs):
+    try:
+        # 取引所別のfactory methodがあればそちらを呼び出す
+        module = _import_module(store)
+        return _build_plugin(store, module, name, **kwargs)
+    except RuntimeError as e:
+        if default_cls is not None:
+            return default_cls(store, **kwargs)
+        else:
+            raise e
+
+
+def timebar(
+    store: DataStoreManagerWrapper,
+    *,
+    seconds: int,
+    maxlen: int = 9999,
+    df: pd.DataFrame = None,
+    callback: Callable[[pd.DataFrame], dict] = None,
+    message_delay: int = 2,
+) -> TimeBarStreamDataFrame:
+    return _import_and_build_plugin(
+        store,
+        "timebar",
+        default_cls=TimeBarStreamDataFrame,
+        seconds=seconds,
+        maxlen=maxlen,
+        df=df,
+        callback=callback,
+        message_delay=message_delay
+    )
+
+
+def volumebar(
+    store: DataStoreManagerWrapper,
+    *,
+    volume_unit: float,
+    maxlen: int = 9999,
+    df: pd.DataFrame = None,
+    callback: Callable[[pd.DataFrame], dict] = None,
+):
+    return _import_and_build_plugin(
+        store,
+        "volumebar",
+        default_cls=VolumeBarStreamDataFrame,
+        volume_unit=volume_unit,
+        maxlen=maxlen,
+        df=df,
+        callback=callback
+    )
+
+
+def binningbook(
+    store: DataStoreManagerWrapper,
+    *,
+    min_bin: int = 0,
+    max_bin: int = 10000000,
+    pips: int = 1,
+    precision: int = 10,
+) -> BinningBook:
+    return _import_and_build_plugin(
+        store,
+        "binningbook",
+        default_cls=BinningBook,
+        min_bin=min_bin,
+        max_bin=max_bin,
+        pips=pips,
+        precision=precision
+    )
+
+
+def bookticker(store: DataStoreManagerWrapper) -> BookTicker:
+    return _import_and_build_plugin(store, "bookticker", default_cls=BookTicker)
--- pybotters_wrapper/plugins/_base.py
+++ pybotters_wrapper/plugins/_base.py
@@ -1,41 +1,82 @@
-from typing import Generic, TypeVar
+from typing import Generic, NamedTuple, TypeVar
 
 import asyncio
 
-from pybotters.store import DataStore, StoreChange
+from pybotters.store import Item, DataStore, StoreChange
 
 
-T = TypeVar('T', bound=DataStore)
 
-
-class WatchPlugin(Generic[T]):
-    def __init__(self, store: T):
+class DataStorePlugin:
+    def __init__(self, store: DataStore):
         self._store = store
-        self._task = asyncio.create_task(self._run())
+        self._wait_task = asyncio.create_task(self._run_wait_task())
+        self._watch_task = asyncio.create_task(self._run_watch_task())
 
-    async def _run(self):
+    async def _run_wait_task(self):
+        while True:
+            await self._store.wait()
+
+    async def _run_watch_task(self):
         with self._store.watch() as stream:
-            async for msg in stream:
-                self._update(msg)
+            async for change in stream:
+                self._on_watch(change)
 
-    def update(self, d: dict, op: str, **kwargs):
-        raise NotImplementedError
+    def on_wait(self):
+        ...
 
-    def _update(self, item: 'StoreChange'):
-        # inplaceで変えないようにcopyする
-        d = self._transform({**item.data}, item.operation)
-        return self.update(d, item.operation)
+    def _on_watch(self, change: StoreChange):
+        transformed_data = self.on_watch_transform({**change.data}, change.operation)
+        self.on_watch(transformed_data, change.operation)
 
-    def _transform(self, d: dict, op: str) -> dict:
-        pass
+    def on_watch(self, d: dict, op: str):
+        ...
 
-    def cancel(self):
-        return self._task.cancel()
+    def on_watch_transform(self, d: dict, op: str) -> dict:
+        return d
 
-    @property
-    def store(self) -> T:
-        return self._store
 
-    @property
-    def task(self):
-        return self._task
+class MultipleDataStoresPlugin:
+    def __init__(self, *stores: DataStore):
+        self._stores = stores
+        self._wait_queue = asyncio.Queue()
+        self._watch_queue = asyncio.Queue()
+        self._wait_task = asyncio.create_task(self._run_wait_task())
+        self._watch_task = asyncio.create_task(self._run_watch_task())
+
+    async def _run_wait_task_one(self, store: 'DataStore'):
+        while True:
+            await store.wait()
+            self._wait_queue.put_nowait(store)
+
+    async def _run_watch_task_one(self, store: 'DataStore'):
+        with store.watch() as stream:
+            async for change in stream:
+                self._watch_queue.put_nowait((store, change))
+
+    async def _run_wait_task(self):
+        [asyncio.create_task(self._run_wait_task_one(s)) for s in self._stores]
+        while True:
+            store = await self._wait_queue.get()
+            self.on_wait(store)
+
+    async def _run_watch_task(self):
+        [asyncio.create_task(self._run_watch_task_one(s)) for s in self._stores]
+        while True:
+            store, change = await self._watch_queue.get()
+            self._on_watch(store, change)
+
+    def on_wait(self, store: 'DataStore'):
+        ...
+
+    def _on_watch(self, store, change: StoreChange):
+        transformed_data = self.on_watch_transform(
+            store, {**change.data}, change.operation
+        )
+        self.on_watch(store, transformed_data, change.operation)
+
+    def on_watch(self, store: 'DataStore', d: dict, op: str):
+        ...
+
+    def on_watch_transform(self, store: 'DataStore', d: dict, op: str) -> dict:
+        return d
+
