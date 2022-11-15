c1c4bb379189c1d83cd032ada70846194efb069d
ko0hi <ko0hi.4731@gmail.com>
2022-11-15 17:03:05 +0900

    impl kucoin api

--- pybotters_wrapper/_apis.py
+++ pybotters_wrapper/_apis.py
@@ -41,6 +41,8 @@ EXCHANGE2API: dict[str, Type[API]] = {
     "binanceusdsm": pbw.binance.BinanceUSDSMAPI,
     "binancecoinm": pbw.binance.BinanceCOINMAPI,
     "bitflyer": pbw.bitflyer.bitFlyerAPI,
+    "kucoinspot": pbw.kucoin.KuCoinSpotAPI,
+    "kucoinfutures": pbw.kucoin.KuCoinFuturesAPI,
     "ftx": pbw.ftx.FTXAPI,
 }
 
---
+++ pybotters_wrapper/kucoin/api.py
@@ -0,0 +1,85 @@
+import uuid
+
+import pybotters
+from pybotters_wrapper.common import API
+
+
+class KuCoinAPIBase(API):
+    _ORDER_ENDPOINT = "/api/v1/orders"
+    _ORDER_ID_KEY = "data.orderId"
+
+    def _make_cancel_endpoint(self, symbol: str, order_id: str, **kwargs):
+        return super()._make_cancel_endpoint(symbol, order_id, **kwargs) + f"/{order_id}"
+
+    def _make_cancel_order_data(
+        self, endpoint: str, symbol: str, order_id: str
+    ) -> None:
+        return None
+
+
+class KuCoinSpotAPI(KuCoinAPIBase):
+    BASE_URL = "https://api.kucoin.com"
+
+    def _make_market_order_data(
+        self, endpoint: str, symbol: str, side: str, size: float
+    ) -> dict:
+        return {
+            "symbol": symbol,
+            "side": side.lower(),
+            "type": "market",
+            "clientOid": str(uuid.uuid4()),
+        }
+
+    def _make_limit_order_data(
+        self,
+        endpoint: str,
+        symbol: str,
+        side: str,
+        price: float,
+        size: float,
+    ) -> dict:
+        return {
+            "symbol": symbol,
+            "side": side.lower(),
+            "type": "limit",
+            "price": price,
+            "size": f"{size:.8f}",
+            "clientOid": str(uuid.uuid4()),
+        }
+
+
+class KuCoinFuturesAPI(KuCoinAPIBase):
+    BASE_URL = "https://api-futures.kucoin.com"
+
+    def __init__(self, client: pybotters.Client, leverage=1, **kwargs):
+        super(KuCoinFuturesAPI, self).__init__(client, **kwargs)
+        self._leverage = leverage
+
+    def _make_market_order_data(
+        self, endpoint: str, symbol: str, side: str, size: float
+    ) -> dict:
+        return {
+            "symbol": symbol,
+            "side": side.lower(),
+            "type": "market",
+            "clientOid": str(uuid.uuid4()),
+            "leverage": self._leverage,
+        }
+
+    def _make_limit_order_data(
+        self,
+        endpoint: str,
+        symbol: str,
+        side: str,
+        price: float,
+        size: float,
+    ) -> dict:
+        return {
+            "symbol": symbol,
+            "side": side.lower(),
+            "type": "limit",
+            "price": price,
+            "size": f"{size:.0f}",
+            "clientOid": str(uuid.uuid4()),
+            "leverage": self._leverage,
+        }
