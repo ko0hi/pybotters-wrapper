5e9f48b464b2dcc25dd7637461fab605187f59ec
ko0hi <ko0hi.4731@gmail.com>
2022-11-20 09:27:49 +0900

    introduce _CSVWriter

--- pybotters_wrapper/plugins/writer.py
+++ pybotters_wrapper/plugins/writer.py
@@ -12,6 +12,52 @@ class WriterMixin:
         raise NotImplementedError
 
 
+class _CSVWriter:
+    def __init__(
+        self,
+        path: str,
+        per_day: bool,
+        columns: list[str] = None
+    ):
+        self._path = path
+        self._columns = columns
+        self._filename = os.path.basename(self._path)
+        self._filepath = os.path.dirname(self._path)
+        self._per_day = per_day
+        self._f: io.TextIOWrapper = None
+        self._writer: csv.DictWriter = None
+
+    def _write(self, d: dict):
+        self._writer.writerow(d)
+
+    def set_columns(self, columns: list[str]):
+        self._columns = columns
+
+    def _get_filepath(self):
+        if self._per_day:
+            day = datetime.utcnow().strftime("%Y-%m-%d")
+            return os.path.join(self._filepath, f"{day}-{self._filename}")
+        else:
+            return self._path
+
+    def _init_or_update_writer(self):
+        filepath = self._get_filepath()
+
+        if self._writer is None:
+            self._init_writer(filepath)
+
+        elif filepath != self._f.name:
+            self._f.close()
+            self._init_writer(filepath)
+
+    def _init_writer(self, filepath: str):
+        if self._columns is None:
+            raise RuntimeError(f"`_columns` has not been initialized yet.")
+        self._f = open(filepath, "w")
+        self._writer = csv.DictWriter(self._f, fieldnames=self._columns)
+        self._writer.writeheader()
+
+
 class DataStoreWatchWriter(DataStorePlugin, WriterMixin):
     def __init__(
         self,
@@ -54,38 +100,13 @@ class DataStoreWatchCSVWriter(DataStoreWatchWriter):
         super(DataStoreWatchCSVWriter, self).__init__(
             store, store_name, columns=columns, operations=operations
         )
-        self._path = path
-        self._filename = os.path.basename(self._path)
-        self._filepath = os.path.dirname(self._path)
-        self._per_day = per_day
-        self._f: io.TextIOWrapper = None
-        self._writer: csv.DictWriter = None
+        self._writer: _CSVWriter = _CSVWriter(path, per_day)
 
     def on_watch_before(self, change: "StoreChange"):
         super().on_watch_before(change)
-        self._init_or_update_writer()
+        if self._writer._columns is None:
+            self._writer.set_columns(self._columns)
+        self._writer._init_or_update_writer()
 
-    def _write(self, item: dict):
-        self._writer.writerow(item)
-
-    def _get_filepath(self):
-        if self._per_day:
-            day = datetime.utcnow().strftime("%Y-%m-%d")
-            return os.path.join(self._filepath, f"{day}-{self._filename}")
-        else:
-            return self._path
-
-    def _init_or_update_writer(self):
-        filepath = self._get_filepath()
-
-        if self._writer is None:
-            self._init_writer(filepath)
-
-        elif filepath != self._f.name:
-            self._f.close()
-            self._init_writer(filepath)
-
-    def _init_writer(self, filepath: str):
-        self._f = open(filepath, "w")
-        self._writer = csv.DictWriter(self._f, fieldnames=self._columns)
-        self._writer.writeheader()
+    def _write(self, d: dict):
+        self._writer._write(d)
