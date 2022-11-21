5a44bf93104862955635596399078105bcb72e20
ko0hi <ko0hi.4731@gmail.com>
2022-11-22 01:30:10 +0900

    add read_resource

--- pybotters_wrapper/utils/io.py
+++ pybotters_wrapper/utils/io.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def read_json(path: str):
@@ -9,4 +10,16 @@ def read_json(path: str):
 def write_json(path: str, obj: any):
     with open(path, "w") as f:
         json.dump(obj, f, indent=4)
+
+
+RESOURCE_PATH = os.path.join(os.path.dirname(__file__), "../resources")
+
+
+def read_resource(filename):
+    filepath = os.path.join(RESOURCE_PATH, filename)
+    if filename.endswith(".json"):
+        return read_json(filepath)
+    else:
+        with open(filepath) as f:
+            return f.readlines()
 
