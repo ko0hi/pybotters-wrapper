8a728384ee3c0017e9703604b4b3cd9fdaafc52b
ko0hi <ko0hi.4731@gmail.com>
2022-11-21 14:48:17 +0900

    add log_command_args helper

--- pybotters_wrapper/utils/__init__.py
+++ pybotters_wrapper/utils/__init__.py
@@ -1,4 +1,4 @@
 from .bucket import Bucket
 from .stream_dataframe import StreamDataFrame
-from .logging import init_logdir, init_logger, logger, LoggingMixin
+from .logging import init_logdir, init_logger, logger, LoggingMixin, log_command_args
 
