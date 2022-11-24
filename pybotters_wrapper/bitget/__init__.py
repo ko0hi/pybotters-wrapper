66163ffdd904d80dd96853aba0a8b4854b5e4bb0
ko0hi <ko0hi.4731@gmail.com>
2022-11-24 22:00:16 +0900

    add BitgetAPI

--- pybotters_wrapper/bitget/__init__.py
+++ pybotters_wrapper/bitget/__init__.py
@@ -1,3 +1,4 @@
+from .api import BitgetAPI
 from .socket import BitgetWebsocketChannels
 from .store import BitgetDataStoreWrapper
 from . import plugins
---
+++ pybotters_wrapper/bitget/api.py
@@ -0,0 +1,4 @@
+from pybotters_wrapper.common import API
+
+class BitgetAPI(API):
+    BASE_URL = "https://api.bitget.com"
