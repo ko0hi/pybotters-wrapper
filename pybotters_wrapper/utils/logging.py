a34759be949eaac94ad34dde6b2a08f5fac92e5f
ko0hi <ko0hi.4731@gmail.com>
2022-11-28 00:02:24 +0900

    move LoggingMixin

--- pybotters_wrapper/utils/logging.py
+++ pybotters_wrapper/utils/logging.py
@@ -55,10 +55,6 @@ def init_logger(
 init_logger()
 
 
-class LoggingMixin:
-    def log(self, msg, level="debug", verbose=True):
-        if verbose:
-            getattr(logger, level)(f"[{self.__class__.__name__}] {msg}")
 
 
 def log_command_args(
