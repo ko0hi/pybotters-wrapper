d58816fbe2cbf098293f6b30d28bc21d38946360
ko0hi <ko0hi.4731@gmail.com>
2022-11-28 00:40:13 +0900

    implement format_precision/format_size in API

--- pybotters_wrapper/common/api.py
+++ pybotters_wrapper/common/api.py
@@ -8,7 +8,21 @@ import requests
 from loguru import logger
 from pybotters.auth import Auth
 
+from pybotters_wrapper.utils import read_resource
 from pybotters_wrapper.utils.mixins import ExchangeMixin, LoggingMixin
+
+
+PRICE_PRECISIONS, SIZE_PRECISIONS = {}, {}
+
+
+def load_precisions(exchange):
+    global PRICE_PRECISIONS, SIZE_PRECISIONS
+    try:
+        PRICE_PRECISIONS[exchange] = read_resource(f"{exchange}_price_precision.json")
+        SIZE_PRECISIONS[exchange] = read_resource(f"{exchange}_size_precision.json")
+    except FileNotFoundError:
+        PRICE_PRECISIONS[exchange] = {}
+        SIZE_PRECISIONS[exchange] = {}
 
 
 class OrderResponse(NamedTuple):
@@ -163,11 +177,27 @@ class API(ExchangeMixin, LoggingMixin):
         wrapped_resp = self._make_cancel_order_response(resp, resp_data, order_id)
         return wrapped_resp
 
+    def format_precision(self, symbol: str, value: float, kind: str):
+        if self.exchange not in PRICE_PRECISIONS:
+            load_precisions(self.exchange)
+
+        if kind == "price":
+            precisions = PRICE_PRECISIONS[self.exchange]
+        elif kind == "size":
+            precisions = SIZE_PRECISIONS[self.exchange]
+        else:
+            raise RuntimeError(f"Unsupported: {kind}")
+
+        if symbol in precisions:
+            return round(value, precisions[symbol])
+        else:
+            return str(value)
+
     def format_price(self, symbol: str, price: float):
-        return str(price)
+        return self.format_precision(symbol, price, "price")
 
     def format_size(self, symbol: str, size: float):
-        return str(size)
+        return self.format_precision(symbol, size, "size")
 
     def _attach_base_url(self, url, base_url: str = None) -> str:
         base_url = base_url or self.BASE_URL
