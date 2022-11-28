bbe76d15412eea7833cdebbcd441513857a9ebbb
pe <105577066+ko0hi@users.noreply.github.com>
2022-11-28 16:33:18 +0900

    WebsocketChannelsのリファクタ (#18)
    * update bitget websocket channels
    
    * delete unused methods
    
    * improve bybit websocket channels
    
    * fix binance websocket channels
    
    * fix GMOWebsocketChannels
    
    * fix kucoin websocket channels

--- pybotters_wrapper/bybit/socket.py
+++ pybotters_wrapper/bybit/socket.py
@@ -8,23 +8,38 @@ class BybitWebsocketChannels(WebsocketChannels):
         request = {"op": "subscribe", "args": list(args)}
         return self.ENDPOINT, request
 
-    def ticker(self, symbol: str, **kwargs):
-        return self.instrument(symbol, **kwargs)
+    def ticker(self, symbol: str, **kwargs) -> BybitWebsocketChannels:
+        return self.instrument_info(symbol)
 
-    def trades(self, symbol: str, **kwargs):
+    def trades(self, symbol: str, **kwargs) -> BybitWebsocketChannels:
         return self.trade(symbol)
 
-    def orderbook(self, symbol: str, **kwargs):
+    def orderbook(self, symbol: str, **kwargs) -> BybitWebsocketChannels:
         return self.orderbook_l2_200(symbol, 100)
 
-    def instrument(self, symbol: str):
+    def order(self, **kwargs) -> BybitWebsocketChannels:
+        return self._subscribe("order")
+
+    def execution(self, **kwargs) -> BybitWebsocketChannels:
+        return self._subscribe("execution")
+
+    def position(self, **kwargs) -> BybitWebsocketChannels:
+        return self._subscribe("position")
+
+    def instrument_info(self, symbol: str) -> BybitWebsocketChannels:
         return self._subscribe(f"instrument_info.100ms.{symbol}")
 
-    def trade(self, symbol: str):
+    def trade(self, symbol: str) -> BybitWebsocketChannels:
         return self._subscribe(f"trade.{symbol}")
 
-    def orderbook_l2_200(self, symbol: str, n: int):
+    def orderbook_l2_200(self, symbol: str, n: int) -> BybitWebsocketChannels:
         return self._subscribe(f"orderBook_200.{n}ms.{symbol}")
+
+    def stop_order(self) -> BybitWebsocketChannels:
+        return self._subscribe("stop_order")
+
+    def wallet(self) -> BybitWebsocketChannels:
+        return self._subscribe("wallet")
 
 
 class BybitUSDTWebsocketChannels(BybitWebsocketChannels):
@@ -32,6 +47,25 @@ class BybitUSDTWebsocketChannels(BybitWebsocketChannels):
     PUBLIC_ENDPOINT = ENDPOINT
     PRIVATE_ENDPOINT = "wss://stream.bybit.com/realtime_private"
 
+    def candle(
+        self, symbol: str, interval: int | str = 1
+    ) -> BybitUSDTWebsocketChannels:
+        return self._subscribe(f"candle.{interval}.{symbol}")
+
+    def liquidation(self, symbol: str) -> BybitUSDTWebsocketChannels:
+        return self._subscribe(f"liquidation.{symbol}")
+
 
 class BybitInverseWebsocketChannels(BybitWebsocketChannels):
     ENDPOINT = "wss://stream.bybit.com/realtime"
+
+    def insurance(self) -> BybitInverseWebsocketChannels:
+        return self._subscribe("insurance")
+
+    def kline_v2(
+        self, symbol: str, interval: int | str = 1
+    ) -> BybitInverseWebsocketChannels:
+        return self._subscribe(f"klineV2.{interval}.{symbol}")
+
+    def liquidation(self) -> BybitInverseWebsocketChannels:
+        return self._subscribe("liquidation")
--- pybotters_wrapper/gmocoin/socket.py
+++ pybotters_wrapper/gmocoin/socket.py
@@ -20,62 +20,26 @@ class GMOWebsocketChannels(WebsocketChannels):
 
         return endpoint, params
 
-    # common channel methods
-    def ticker(self, symbol, **kwargs):
+    def ticker(self, symbol, **kwargs) -> GMOWebsocketChannels:
         return self._subscribe("ticker", symbol=symbol)
 
-    def trades(self, symbol, option="TAKER_ONLY", **kwargs):
+    def trades(self, symbol, option="TAKER_ONLY", **kwargs) -> GMOWebsocketChannels:
         return self._subscribe("trades", symbol=symbol, option=option)
 
-    def orderbook(self, symbol, **kwargs):
+    def orderbook(self, symbol, **kwargs) -> GMOWebsocketChannels:
         return self.orderbooks(symbol)
 
-    # GMO channel methods
-    def orderbooks(self, symbol):
+    def orderbooks(self, symbol) -> GMOWebsocketChannels:
         return self._subscribe("orderbooks", symbol=symbol)
 
-    def execution_events(self):
+    def execution_events(self) -> GMOWebsocketChannels:
         return self._subscribe("executionEvents")
 
-    def order_events(self):
+    def order_events(self) -> GMOWebsocketChannels:
         return self._subscribe("orderEvents")
 
-    def position_events(self):
+    def position_events(self) -> GMOWebsocketChannels:
         return self._subscribe("positionEvents")
 
-    def position_summary_events(self):
+    def position_summary_events(self) -> GMOWebsocketChannels:
         return self._subscribe("positionSummaryEvents")
-
-    def public_channels(self, symbol):
-        return [self.ticker(symbol), self.orderbooks(symbol), self.trades(symbol)]
-
-    def private_channels(self):
-        return [
-            self.execution_events(),
-            self.order_events(),
-            self.position_events(),
-            self.position_summary_events(),
-        ]
-
-    def all_channels(self, symbol):
-        return self.public_channels(symbol) + self.private_channels()
-
-    def get_public_channels(self, send_json):
-        return [
-            sj
-            for sj in send_json
-            if sj["channel"] in ("ticker", "orderbooks", "trades")
-        ]
-
-    def get_private_channels(self, send_json):
-        return [
-            sj
-            for sj in send_json
-            if sj["channel"]
-               in (
-                   "executionEvents",
-                   "orderEvents",
-                   "positionEvents",
-                   "positionSummaryEvents",
-               )
-        ]
