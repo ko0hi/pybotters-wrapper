33ad28f034b234c94dc7ddc0855c1ab99fe92ea6
ko0hi <ko0hi.4731@gmail.com>
2022-12-01 01:21:34 +0900

    add sample for websocket modules

---
+++ sample/misc/functional_use_of_websocket_connection.py
@@ -0,0 +1,49 @@
+import asyncio
+import pybotters
+import pybotters_wrapper as pbw
+
+
+async def main():
+    symbol = "FX_BTC_JPY"
+
+    async with pybotters.Client() as client:
+        endpoint = "wss://ws.lightstream.bitflyer.com/json-rpc"
+        send = [
+            {
+                "method": "subscribe",
+                "params": {"channel": f"lightning_ticker_{symbol}"},
+                "id": 1,
+            },
+            {
+                "method": "subscribe",
+                "params": {"channel": f"lightning_executions_{symbol}"},
+                "id": 2,
+            },
+            {
+                "method": "subscribe",
+                "params": {"channel": f"lightning_board_snapshot_{symbol}"},
+                "id": 3,
+            },
+            {
+                "method": "subscribe",
+                "params": {"channel": f"lightning_board_{symbol}"},
+                "id": 4,
+            },
+            {
+                "method": "subscribe",
+                "params": {"channel": "child_order_events"},
+                "id": 5,
+            },
+        ]
+
+        hdlr = lambda msg, ws: print(msg)
+
+        connection = pbw.common.WebsocketConnection(endpoint, send, hdlr)
+        await connection.connect(client, auto_reconnect=True)
+
+        while True:
+            await asyncio.sleep(1)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
