7a925c7cab8894aa6509b7619a69b2212f4f48c0
ko0hi <ko0hi.4731@gmail.com>
2022-11-30 22:36:49 +0900

    NormalizedStoreの実装

--- pybotters_wrapper/gmocoin/store.py
+++ pybotters_wrapper/gmocoin/store.py
@@ -4,8 +4,14 @@ import pandas as pd
 from pybotters.models.gmocoin import GMOCoinDataStore
 from pybotters_wrapper.common.store import (
     DataStoreWrapper,
+    ExecutionItem,
+    ExecutionStore,
     OrderbookItem,
     OrderbookStore,
+    OrderItem,
+    OrderStore,
+    PositionItem,
+    PositionStore,
     TickerItem,
     TickerStore,
     TradesItem,
@@ -39,9 +45,43 @@ class GMOCoinOrderbookStore(OrderbookStore):
         )
 
 
+class GMOCoinOrderStore(OrderStore):
+    def _normalize(self, d: dict, op: str) -> "OrderItem":
+        return self._itemize(
+            str(d["orderId"]),
+            d["symbol"].name,
+            d["side"].name,
+            float(d["orderPrice"]),
+            float(d["orderSize"]),
+            d["executionType"],
+        )
+
+
+class GMOCoinExecutionStore(ExecutionStore):
+    def _normalize(self, d: dict, op: str) -> "ExecutionItem":
+        return self._itemize(
+            str(d["orderId"]),
+            d["symbol"].name,
+            d["side"].name,
+            float(d["executionPrice"]),
+            float(d["executionSize"]),
+            pd.to_datetime(d["executionTimestamp"]),
+        )
+
+
+class GMOCoinPositionStore(PositionStore):
+    def _normalize(self, d: dict, op: str) -> "PositionItem":
+        return self._itemize(
+            d["symbol"].name, d["side"].name, float(d["price"]), float(d["size"])
+        )
+
+
 class GMOCoinDataStoreWrapper(GMOCoinMixin, DataStoreWrapper[GMOCoinDataStore]):
     _WRAP_STORE = GMOCoinDataStore
     _WEBSOCKET_CHANNELS = GMOWebsocketChannels
     _TICKER_STORE = (GMOCoinTickerStore, "ticker")
     _TRADES_STORE = (GMOCoinTradesStore, "trades")
     _ORDERBOOK_STORE = (GMOCoinOrderbookStore, "orderbooks")
+    _ORDER_STORE = (GMOCoinOrderStore, "orders")
+    _EXECUTION_STORE = (GMOCoinExecutionStore, "executions")
+    _POSITION_STORE = (GMOCoinPositionStore, "positions")
