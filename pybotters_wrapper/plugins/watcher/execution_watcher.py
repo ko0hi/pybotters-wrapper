a1948deeffba1b51b2e007f292412c5fce5dbb76
ko0hi <ko0hi.4731@gmail.com>
2022-12-02 01:17:44 +0900

    clean ExecutionWatcher

--- pybotters_wrapper/plugins/watcher/execution_watcher.py
+++ pybotters_wrapper/plugins/watcher/execution_watcher.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 
 import asyncio
 
+from ...core.store import ExecutionItem
 from .._base import DataStorePlugin
 
 
 class ExecutionWatcher(DataStorePlugin):
     def __init__(self, store: "DataStoreWrapper"):
+        # ExecutionDataStoreを監視
         super(ExecutionWatcher, self).__init__(store.execution)
         self._order_id = None
         self._item = None
@@ -14,6 +16,7 @@ class ExecutionWatcher(DataStorePlugin):
         self._event = asyncio.Event()
 
     def set(self, order_id: str) -> ExecutionWatcher:
+        """監視対象の注文IDをセット"""
         if self._order_id is not None:
             raise RuntimeError(
                 f"ExecutionWatcher must not be 'reused', create a new instance instead."
@@ -22,7 +25,12 @@ class ExecutionWatcher(DataStorePlugin):
         self._event.set()
         return self
 
-    async def _on_watch(self, d: dict, op: str):
+    async def _on_watch(self, d: ExecutionItem, op: str):
+        """流れてきた約定情報が監視中の注文に関するものであるかをチェック
+
+        ExecutionStoreを監視してるので、流れてくるdictはExecutionItem
+
+        """
         if not self._event.is_set():
             # order_idがsetされるまで待機
             # 注文が即約定した時にsocket messageがresのresponseより早く到達するケースがあるので、
@@ -34,17 +42,18 @@ class ExecutionWatcher(DataStorePlugin):
             self._item = d
 
     def _on_watch_is_stop(self, d: dict, op: str) -> bool:
+        """監視終了判定"""
         return self._done
 
-    def done(self):
+    def done(self) -> bool:
         return self._done
 
-    def result(self):
+    def result(self) -> ExecutionItem:
         return self._item
 
     async def wait(self):
         return await self._watch_task
 
     @property
-    def order_id(self):
+    def order_id(self) -> str:
         return self._order_id
