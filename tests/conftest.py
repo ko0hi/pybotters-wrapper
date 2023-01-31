dc96688bbbac73f49a19d6e8ee75a43e246fbaf7
pe <105577066+ko0hi@users.noreply.github.com>
2023-02-01 01:12:04 +0900

    initializationのリファクタ (#90)
    * InitializeRequestConfigをnamedtupleに変更
    
    * initializeのvalidationを関数化
    
    * _initialize_with_validationの引数をawaitableに変更
    
    * initializeを掃除
    
    * minor
    
    * initialize helper methodの整備
    
    * delete old helper
    
    * null除外をヘルパー内で実施
    
    * 各種initialize conf用のメソッドを追加
    
    * エラーメッセージを改良
    
    * null conf handling
    
    * _initialize_requestにkwargsを追加
    
    * binancespotのauthの問題に対処
    
    * テスト追加
    
    * sampleを追加

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
+import pytest_asyncio
 
 from unittest import TestCase
+
+
+import pybotters
+import pybotters_wrapper as pbw
 
 
 @pytest.fixture
@@ -8,3 +13,8 @@ def testcase() -> TestCase:
     testcase = TestCase()
     testcase.maxDiff = None
     return testcase
+
+
+@pytest_asyncio.fixture
+async def client() -> pybotters.Client:
+    return pbw.create_client()
