f0cf5dcc9905f37420c1a287ad89352ec8adbc6f
ko0hi <ko0hi.4731@gmail.com>
2023-03-02 00:30:21 +0900

    _maybe_override_by_exchangeの廃止
    Signed-off-by: ko0hi <ko0hi.4731@gmail.com>

--- pybotters_wrapper/plugins/_apis.py
+++ pybotters_wrapper/plugins/_apis.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
-import importlib
-import types
-from typing import Callable, Type
+from typing import Callable
 
 import pandas as pd
 import pybotters
 
 from pybotters_wrapper.core import API, DataStoreWrapper
-from ._base import Plugin
 from .influxdb import InfluxDB
 from .market import (
     BinningBook,
@@ -20,47 +17,11 @@ from .market.bar import BarStreamDataFrame
 from .periodic import Poller
 from .status import PnL
 from .watcher import ExecutionWatcher
-from .writer import BarCSVWriter, DataStoreWaitCSVWriter, DataStoreWatchCSVWriter
-
-import_cache = {}
-
-
-def _import_module(store: DataStoreWrapper) -> types.ModuleType:
-    """取引所固有のプラグインのmoduleを読み込む。読み込んだものはキャッシュする。"""
-    try:
-        module_path = f"pybotters_wrapper.{store.package}.plugins.{store.exchange}"
-        if module_path in import_cache:
-            return import_cache[module_path]
-        module = importlib.import_module(module_path)
-        import_cache[module_path] = module
-        return module
-    except ModuleNotFoundError:
-        raise RuntimeError(f"no plugins module for {store.exchange}")
-
-
-def _build_plugin(
-    store: DataStoreWrapper, module: types.ModuleType, name: str, **kwargs
-) -> Plugin:
-    """取引所固有のプラグイン実装をインスタンス化する"""
-    try:
-        return getattr(module, name)(store, **kwargs)
-    except AttributeError:
-        raise RuntimeError(
-            f"'{name}' plugin has not been supported yet for {store.exchange}"
-        )
-
-
-def _maybe_override_by_exchange(
-    store: DataStoreWrapper, plugin_class: Type[Plugin], **kwargs
-) -> Plugin:
-    """取引所別の実装クラスがあればそちらを呼び出す。取引所別の実装は同名クラスが
-    "pybotters_wrapper/${package}/plugins/${exchange}/__init__.pyで定義されている想定
-    """
-    try:
-        module = _import_module(store)
-        return _build_plugin(store, module, plugin_class.__name__, **kwargs)
-    except RuntimeError:
-        return plugin_class(store, **kwargs)  # noqa
+from .writer import (
+    BarCSVWriter,
+    DataStoreWaitCSVWriter,
+    DataStoreWatchCSVWriter,
+)
 
 
 # pluginのファクトリーメソッド
@@ -73,9 +34,8 @@ def timebar(
     callback: Callable[[pd.DataFrame], dict] = None,
     message_delay: int = 2,
 ) -> TimeBarStreamDataFrame:
-    return _maybe_override_by_exchange(
+    return TimeBarStreamDataFrame(
         store,
-        TimeBarStreamDataFrame,
         seconds=seconds,
         maxlen=maxlen,
         df=df,
@@ -92,9 +52,8 @@ def volumebar(
     df: pd.DataFrame = None,
     callback: Callable[[pd.DataFrame], dict] = None,
 ) -> VolumeBarStreamDataFrame:
-    return _maybe_override_by_exchange(
+    return VolumeBarStreamDataFrame(
         store,
-        VolumeBarStreamDataFrame,
         volume_unit=volume_unit,
         maxlen=maxlen,
         df=df,
@@ -110,9 +69,8 @@ def binningbook(
     pips: int = 1,
     precision: int = 10,
 ) -> BinningBook:
-    return _maybe_override_by_exchange(
+    return BinningBook(
         store,
-        BinningBook,
         min_bin=min_bin,
         max_bin=max_bin,
         pips=pips,
@@ -121,7 +79,7 @@ def binningbook(
 
 
 def bookticker(store: DataStoreWrapper) -> BookTicker:
-    return _maybe_override_by_exchange(store, BookTicker)
+    return BookTicker(store)
 
 
 def poller(
@@ -134,7 +92,9 @@ def poller(
     history: int = 999,
     method: str = "GET",
 ) -> Poller:
-    return Poller(api_or_client, url, interval, params, handler, history, method)
+    return Poller(
+        api_or_client, url, interval, params, handler, history, method
+    )
 
 
 def execution_watcher(
@@ -143,13 +103,11 @@ def execution_watcher(
     store_name: str = None,
     is_target: Callable[["DataStore", str, dict, dict], bool] = None,
 ) -> ExecutionWatcher:
-    return _maybe_override_by_exchange(
-        store, ExecutionWatcher, store_name=store_name, is_target=is_target
-    )
+    return ExecutionWatcher(store, store_name=store_name, is_target=is_target)
 
 
 def pnl(store: DataStoreWrapper, symbol: str, fee: float = 0.0) -> PnL:
-    return _maybe_override_by_exchange(store, PnL, symbol=symbol, fee=fee)
+    return PnL(store, symbol=symbol, fee=fee)
 
 
 def watch_csvwriter(
@@ -162,9 +120,8 @@ def watch_csvwriter(
     flush: bool = False,
     operations: list[str] = None,
 ) -> DataStoreWatchCSVWriter:
-    return _maybe_override_by_exchange(
+    return DataStoreWatchCSVWriter(
         store,
-        DataStoreWatchCSVWriter,
         store_name=store_name,
         path=path,
         per_day=per_day,
@@ -183,9 +140,8 @@ def wait_csvwriter(
     columns: list[str] = None,
     flush: bool = False,
 ) -> DataStoreWaitCSVWriter:
-    return _maybe_override_by_exchange(
+    return DataStoreWaitCSVWriter(
         store,
-        DataStoreWaitCSVWriter,
         store_name=store_name,
         path=path,
         per_day=per_day,
@@ -195,7 +151,10 @@ def wait_csvwriter(
 
 
 def bar_csvwriter(
-    bar: BarStreamDataFrame, path: str, per_day: bool = False, flush: bool = False
+    bar: BarStreamDataFrame,
+    path: str,
+    per_day: bool = False,
+    flush: bool = False,
 ) -> BarCSVWriter:
     # pluginの上のpluginなので取引所別のオーバーライド必要なし
     return BarCSVWriter(bar, path=path, per_day=per_day, flush=flush)
