43b68b3deca629b0106b839033f27eb6ca17739c
yota-p <930713yh@gmail.com>
2023-03-23 17:10:57 +0000

    delete _buy_price, _sell_price, compute_pnl

--- pybotters_wrapper/plugins/status/pnl.py
+++ pybotters_wrapper/plugins/status/pnl.py
@@ -7,17 +7,6 @@ from .._base import Plugin
 from ..mixins import WatchStoreMixin
 
 
-def compute_pnl(
-    buy_avg_price: float,
-    buy_size: float,
-    sell_avg_price: float,
-    sell_size: float,
-) -> float:
-    # 実現損益のみ
-    size = min(buy_size, sell_size)
-    return sell_avg_price * size - buy_avg_price * size
-
-
 class PnL(WatchStoreMixin, Plugin):
     def __init__(
         self, store: DataStoreWrapper, symbol: str, *, fee: float = 0, snapshot_length=9999
@@ -30,10 +19,8 @@ class PnL(WatchStoreMixin, Plugin):
         self._symbol = symbol
         self._fee = fee
         self._snapshots = deque(maxlen=snapshot_length)
-        self._buy_price = 0
         self._buy_size = 0
         self._buy_volume = 0
-        self._sell_price = 0
         self._sell_size = 0
         self._sell_volume = 0
         self.init_watch_store(store.execution)
@@ -50,11 +37,9 @@ class PnL(WatchStoreMixin, Plugin):
 
     def _update_status(self, side: str, price: float, size: float):
         if side == "BUY":
-            self._buy_price += price
             self._buy_size += size
             self._buy_volume += price * size
         else:
-            self._sell_price += price
             self._sell_size += size
             self._sell_volume += price * size
 
@@ -70,4 +55,3 @@ class PnL(WatchStoreMixin, Plugin):
     @property
     def _volume(self):
         return self._buy_volume + self._sell_volume
-
