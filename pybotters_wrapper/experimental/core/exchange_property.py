4557bc17c4adeb946554b34cfe10d48778b5c895
ko0hi <ko0hi.4731@gmail.com>
2023-04-03 22:37:54 +0900

    ExchangePropertyにbase_urlを追加

--- pybotters_wrapper/experimental/core/exchange_property.py
+++ pybotters_wrapper/experimental/core/exchange_property.py
@@ -3,5 +3,9 @@ from abc import ABCMeta
 
 class ExchangeProperty(metaclass=ABCMeta):
     @property
+    def base_url(self) -> str:
+        raise NotImplementedError
+
+    @property
     def exchange(self) -> str:
         raise NotImplementedError
