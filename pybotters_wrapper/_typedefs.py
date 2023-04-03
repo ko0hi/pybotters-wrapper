ca00b30d31f18080d93f76641556f44151620246
ko0hi <ko0hi.4731@gmail.com>
2023-04-03 18:01:30 +0900

    TDataStoreManager型を追加

--- pybotters_wrapper/_typedefs.py
+++ pybotters_wrapper/_typedefs.py
@@ -1,4 +1,7 @@
-from typing import Literal
+from typing import Literal, TypeVar
+
+from pybotters.store import DataStoreManager
 
 Side = Literal["BUY", "SELL"]
 RequsetMethod = Literal["GET", "POST", "PUT", "DELETE"]
+TDataStoreManager = TypeVar("TDataStoreManager", bound=DataStoreManager)
