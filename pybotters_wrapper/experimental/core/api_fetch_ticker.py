35fb5f1cc5eb950ca4f9df65d8af64b19f51866c
ko0hi <ko0hi.4731@gmail.com>
2023-04-10 23:42:04 +0900

    fetch_tikcer更新

--- pybotters_wrapper/experimental/core/api_fetch_ticker.py
+++ pybotters_wrapper/experimental/core/api_fetch_ticker.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Callable, Awaitable
+from typing import NamedTuple, Callable, Awaitable, TypedDict
 
 from aiohttp.client import ClientResponse
 
@@ -12,12 +12,36 @@ class TickerFetchAPIResponse(NamedTuple):
     resp_data: dict | None = None
 
 
-class TickerFetchAPI(FetchAPI):
+class TickerFetchAPIGenerateEndpointParameters(TypedDict):
+    symbol: TSymbol
+    extra_params: dict
+
+
+class TickerFetchAPITranslateParametersParameters(TypedDict):
+    endpoint: TEndpoint
+    symbol: TSymbol
+    extra_params: dict
+
+
+class TickerFetchAPIWrapResponseParameters(TypedDict):
+    item: TickerItem
+    resp: ClientResponse
+    resp_data: dict
+
+
+class TickerFetchAPI(
+    FetchAPI[
+        TickerFetchAPIResponse,
+        TickerFetchAPIGenerateEndpointParameters,
+        TickerFetchAPITranslateParametersParameters,
+        TickerFetchAPIWrapResponseParameters,
+    ]
+):
     def __init__(
         self,
         api_client: APIClient,
         method: TRequsetMethod,
-        endpoint: TEndpoint | Callable[[TSymbol, dict], str] | None = None,
+        endpoint_generator: TEndpoint | Callable[[TSymbol, dict], str] | None = None,
         parameter_translater: Callable[[TEndpoint, TSymbol, dict], dict] | None = None,
         response_decoder: Callable[
             [ClientResponse], dict | list | Awaitable[dict | list]
@@ -26,24 +50,14 @@ class TickerFetchAPI(FetchAPI):
         response_itemizer: Callable[[ClientResponse, any], TickerItem] | None = None,
     ):
         super(TickerFetchAPI, self).__init__(
-            api_client, method, response_decoder=response_decoder
+            api_client,
+            method,
+            endpoint_generator=endpoint_generator,
+            parameters_translater=parameter_translater,
+            response_wrapper_cls=TickerFetchAPIResponse,
+            response_decoder=response_decoder,
         )
-        self._endpoint = endpoint
-        self._parameter_translater = parameter_translater
         self._response_itemizer = response_itemizer
-
-    def _generate_endpoint(self, symbol: TSymbol, extra_params: dict) -> TEndpoint:
-        assert self._endpoint is not None
-        if isinstance(self._endpoint, str):
-            return self._endpoint
-        elif callable(self._endpoint):
-            return self._endpoint(symbol)
-
-    def _translate_parameters(
-        self, endpoint: TEndpoint, symbol: TSymbol, extra_params: dict
-    ) -> dict:
-        assert self._parameter_translater is not None
-        return self._parameter_translater(endpoint, symbol, extra_params)
 
     def _itemize_response(
         self, resp: ClientResponse, resp_data: any | None = None
@@ -51,20 +65,27 @@ class TickerFetchAPI(FetchAPI):
         assert self._response_itemizer is not None
         return self._response_itemizer(resp, resp_data)
 
-    def _wrap_response(
-        self, item: TickerItem, resp: ClientResponse, resp_data: dict
-    ) -> TickerFetchAPIResponse:
-        return TickerFetchAPIResponse(item, resp, resp_data)
-
     def fetch_ticker(
         self, symbol: TSymbol, *, extra_params: dict = None, request_params: dict = None
     ) -> TickerFetchAPIResponse:
         extra_params = extra_params or {}
         request_params = request_params or {}
-        endpoint = self._generate_endpoint(symbol, extra_params)
-        parameters = self._translate_parameters(endpoint, symbol, extra_params)
+        endpoint = self._generate_endpoint(
+            TickerFetchAPIGenerateEndpointParameters(
+                symbol=symbol, extra_params=extra_params
+            )
+        )
+        parameters = self._translate_parameters(
+            TickerFetchAPITranslateParametersParameters(
+                endpoint=endpoint, symbol=symbol, extra_params=extra_params
+            )
+        )
         parameters = {**parameters, **extra_params}
         resp = await self.request(endpoint, parameters, **request_params)
         resp_data = await self._decode_response(resp)
         item = self._itemize_response(resp, resp_data)
-        return self._wrap_response(item, resp, resp_data)
+        return self._wrap_response(
+            TickerFetchAPIWrapResponseParameters(
+                item=item, resp=resp, resp_data=resp_data
+            )
+        )
