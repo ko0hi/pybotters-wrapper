ea51c6f930e5efd35a0714e913361389e6ae4f1a
ko0hi <ko0hi.4731@gmail.com>
2023-04-11 23:32:03 +0900

    add test: ExchangeProperty

---
+++ tests/test_exchange_property.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+from pybotters_wrapper.core.exchange_property import ExchangeProperty
+
+
+def test_exchange_property():
+    eprop = ExchangeProperty(
+        {"base_url": "sample_base_url", "exchange": "sample_exchange"}
+    )
+    assert eprop.base_url == "sample_base_url"
+    assert eprop.exchange == "sample_exchange"
+
+
+def test_exchange_property_with_invalid_config():
+    with pytest.raises(ValueError) as e:
+        # typehint error should occur
+        ExchangeProperty({"base_url": "sample_base_url"})
+    assert str(e.value) == "Missing properties: ['exchange']"
