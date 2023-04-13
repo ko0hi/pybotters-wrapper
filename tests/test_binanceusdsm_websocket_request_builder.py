96cc908fd71b4f7e88b2fef72164c5ec3c3a0c49
ko0hi <ko0hi.4731@gmail.com>
2023-04-14 01:31:59 +0900

    mock修正

--- tests/test_binanceusdsm_websocket_request_builder.py
+++ tests/test_binanceusdsm_websocket_request_builder.py
@@ -122,11 +122,11 @@ async def test_binanceusdsm_websocket_builder_with_customizer(
     builder, mocker: pytest_mock.MockerFixture
 ):
     mocker.patch("time.monotonic", return_value=1)
+    mocker.patch(
+        "pybotters_wrapper.binance.common.listenkey_fetcher.BinanceListenKeyFetcher.get_listenkey",
+        return_value="pqia91ma19a5s61cv6a81va65sdf19v8a65a1a5s61cv6a81va65sdf19v8a65a1",
+    )
     symbol = "BTCUSDT"
-    listen_key_url = "https://fapi.binance.com/fapi/v1/listenKey"
-    listenkey_dummy_response = {
-        "listenKey": "pqia91ma19a5s61cv6a81va65sdf19v8a65a1a5s61cv6a81va65sdf19v8a65a1"
-    }
     expected = {
         "wss://fstream.binance.com/ws": {
             "id": 1 * 10**9,
@@ -135,18 +135,15 @@ async def test_binanceusdsm_websocket_builder_with_customizer(
                 "btcusdt@ticker",
                 "btcusdt@aggTrade",
                 "btcusdt@depth",
-                "qgQWucD254wiYJQAyJIH0v54rNq77IG1szMjSfNelibNoOYwFHSolsNqLVdiubQX",
+                "pqia91ma19a5s61cv6a81va65sdf19v8a65a1a5s61cv6a81va65sdf19v8a65a1",
             ],
         },
     }
     async with create_client() as client:
         customizer = create_binanceusdsm_websocket_request_customizer()
         customizer.set_client(client)
-        with aioresponses() as m:
-            m.post(listen_key_url, payload=listenkey_dummy_response)
+        actual = builder.subscribe("all", symbol=symbol).get(
+            request_customizer=customizer
+        )
 
-            actual = builder.subscribe("all", symbol=symbol).get(
-                request_customizer=customizer
-            )
-
-            assert actual == expected
+        assert actual == expected
