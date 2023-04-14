d1fdb78388d1f236c12cc65f13e3cfe6f8cb41a1
ko0hi <ko0hi.4731@gmail.com>
2023-04-14 23:52:47 +0900

    add PositionStore test

--- pybotters_wrapper/core/normalized_store_position.py
+++ pybotters_wrapper/core/normalized_store_position.py
@@ -15,36 +15,37 @@ class PositionStore(NormalizedDataStore[PositionItem]):
     _NAME = "position"
     _KEYS = ["symbol"]
 
-    def size(self, side: str, symbol: str = None) -> float:
-        query = {"side": side}
-        if symbol:
-            query["symbol"] = symbol
-        return sum([x["size"] for x in self.find(query)])
+    def size(self, symbol: str, side: str=None) -> float:
+        query = {"symbol": symbol}
+        if side is None:
+            items = self.find(query)
+            return sum([x["size"] if x["side"] == "BUY" else -x["size"] for x in items])
+        else:
+            query["side"] = side
+            return sum([x["size"] for x in self.find(query)])
 
-    def price(self, side: str, symbol: str = None) -> float:
-        query = {"side": side}
-        if symbol:
-            query["symbol"] = symbol
+    def price(self, symbol: str, side: str) -> float:
+        query = {"symbol": symbol}
         positions = self.find(query)
-        size = self.size(side)
+        size = self.size(symbol, side)
         if size > 0:
             return sum([p["price"] * p["size"] for p in positions]) / size
         else:
             return 0
 
-    def summary(self, symbol: str = None):
+    def summary(self, symbol: str) -> dict:
         rtn = {}
         for s in ["BUY", "SELL"]:
-            rtn[f"{s}_size"] = self.size(s, symbol)
-            rtn[f"{s}_price"] = self.price(s, symbol)
+            rtn[f"{s}_size"] = self.size(symbol, s)
+            rtn[f"{s}_price"] = self.price(symbol, s)
 
-        if rtn["BUY_size"] > 0 and rtn["SELL_size"] == 0:
+        rtn["size"] = self.size(symbol)
+
+        if rtn["size"] > 0:
             rtn["side"] = "BUY"
-        elif rtn["BUY_size"] == 0 and rtn["SELL_size"] > 0:
+        elif rtn["size"] < 0:
             rtn["side"] = "SELL"
-        elif rtn["BUY_size"] > 0 and rtn["SELL_size"] > 0:
-            rtn["side"] = "BOTH"
         else:
-            rtn["side"] = None
+            rtn["side"] = "NO"
 
         return rtn
