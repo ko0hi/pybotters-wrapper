687c1c19f5bc4e718458c12c9e1ea164feee8e61
ko0hi <ko0hi.4731@gmail.com>
2023-04-16 00:07:11 +0900

    update test

--- pybotters_wrapper/binance/binanceusdsm/__init__.py
+++ pybotters_wrapper/binance/binanceusdsm/__init__.py
@@ -1,5 +1,6 @@
 from .factory_binanceusdsm import (
     create_binanceusdsm_exchange_property,
+    create_binanceusdsm_store_initializer,
     create_binanceusdsm_websocket_request_builder,
     create_binanceusdsm_websocket_request_customizer,
     create_binanceusdsm_apiclient,
@@ -12,6 +13,6 @@ from .factory_binanceusdsm import (
     create_binanceusdsm_fetch_orderbook_api,
     create_binanceusdsm_fetch_orders_api,
     create_binanceusdsm_fetch_positions_api,
-    create_binanceusdsm_normalized_store_builder
+    create_binanceusdsm_normalized_store_builder,
 )
 from .websocket_channels_binanceusdsm import BinanceUSDSMWebsocketChannels
