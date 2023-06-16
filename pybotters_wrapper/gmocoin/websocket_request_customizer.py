acfbf5d6ad3f2242100a80afc0e3141d462a41f7
ko0hi <ko0hi.4731@gmail.com>
2023-06-17 01:16:19 +0900

    minor

--- pybotters_wrapper/gmocoin/websocket_request_customizer.py
+++ pybotters_wrapper/gmocoin/websocket_request_customizer.py
@@ -7,12 +7,20 @@ from ..core import WebSocketRequestCustomizer
 class GMOCoinWebsocketRequestCustomizer(WebSocketRequestCustomizer):
     def __init__(self, client: pybotters.Client = None):
         super(GMOCoinWebsocketRequestCustomizer, self).__init__(client)
-        self._token_fetcher = GMOCoinTokenFetcher(client)
+        self._token_fetcher: GMOCoinTokenFetcher | None = None
 
     def customize(
         self, endpoint: str, request_list: list[dict | str]
     ) -> tuple[str, list[dict | str]]:
+        if self._token_fetcher is None:
+            self._init_token_fetcher()
+
         if "private" in endpoint:
             return f"{endpoint}/{self._token_fetcher.fetch()}", request_list
         else:
             return endpoint, request_list
+
+    def _init_token_fetcher(self) -> None:
+        if self._client is None:
+            raise RuntimeError("Client is not set")
+        self._token_fetcher = GMOCoinTokenFetcher(self._client)
